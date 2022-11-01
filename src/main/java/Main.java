69907d795a039e02d7b0d0baaef57f9a018863c2
AndrewAscone <andrew.ascone@gmail.com>
2022-11-01 10:21:53 -0400

    Program functions and adds the sum of numbers from 1 to the user's input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+
+        int number = input.nextInt();
+        int total = sumOfNumbers(number);
+
+        System.out.println("The total sum of numbers from 1 to " + number + " is: " + total);
+
+
 
     }
     
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int result = 0;
+        for (int i = 1; i <=n; i++){
+            result += i;
+        }
+        return result;
     }
     
     // for Extra Credit
